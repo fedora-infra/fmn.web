662d7bb4e724647096cd58b042c60c8a0f7e20e5
Jeremy Cline <jeremy@jcline.org>
2016-12-12 16:22:09 -0500

    Add a runserver.py script to the repository root
    There is a fmn/web/main.py module to start the web application in a
    development server. However, it's not configurable without editing the
    file and it also is not particularly useful to anyone except for
    developers. It now prints a deprecation notice and a new
    ``runserver.py`` script has been added to the repository root for
    developer use.
    
    Signed-off-by: Jeremy Cline <jeremy@jcline.org>

--- fmn/web/main.py
+++ fmn/web/main.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 """ The flask application """
 
-## These two lines are needed to run on EL6
-__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
-import pkg_resources
+# These two lines are needed to run on EL6
+__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']  # NOQA
+import pkg_resources  # NOQA
 
 from fmn.web.app import app
 
+
 if __name__ == '__main__':
     app.debug = True
+    print('Running the FMN web application from fmn/web/main.py is deprecated,'
+          ' please use runserver.py in the root of the repository.')
     app.run()
